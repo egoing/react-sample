a91065184c004d0c3776f23546538d01e98146ef
egoing <egoing@gmail.com>
2021-02-02 02:31:07 +0900

    step2 - 컴포넌트 도입

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
+# 이전 작업 
+https://stackblitz.com/edit/react-step-1?file=src/App.js
+
 # 성취
-리액트를 실행할 수 있게 되었습니다. 
+컴포넌트를 만들 수 있게 되었습니다. 
 
 # 불만족
-header, nav, article 태그가 너무 복잡합니다. 
+컴포넌트가 너무 경직 되어 있습니다. HTML 태그 처럼 속성을 통해서 컴포넌트를 커스터마이징 하고 싶습니다. 
 
 # 도약
-각 태그를 Header, Nav, Article 라는 이름의 컴포넌트로 만들어서 단순화 해보세요.
-
-# 다음작업 
-https://stackblitz.com/edit/react-step-2?file=README.md
+아래와 같은 인터페이스를 가지고 있는 컴포넌트를 만들어보세요. 
+```
+<Nav
+  data={[
+    { id: 1, title: "html", description: "html is..." },
+    { id: 2, title: "css", description: "css is..." },
+    { id: 3, title: "js", description: "js is..." }
+  ]}
+/>
+<Article title="제목" description="본문" />
+```
--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 import React from "react";
 import "./style.css";
-
+function Header() {
+  return (
+    <header>
+      <h1>WEB</h1>
+    </header>
+  );
+}
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/1">html</a>
+        </li>
+        <li>
+          <a href="/2">css</a>ewrewr
+        </li>
+        <li>
+          <a href="/3">javascript</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello, WEB!
+    </article>
+  );
+}
 export default function App() {
   return (
     <div>
-      <header>
-        <h1>WEB</h1>
-      </header>
-      <nav>
-        <ul>
-          <li>
-            <a href="/1">html</a>
-          </li>
-          <li>
-            <a href="/2">css</a>
-          </li>
-          <li>
-            <a href="/3">javascript</a>
-          </li>
-        </ul>
-      </nav>
-      <article>
-        <h2>Welcome</h2>
-        Hello, WEB!
-      </article>
+      <Header />
+      <Nav />
+      <Article />
     </div>
   );
 }
