557117e272a9be23ac5d8bde9eee5f7911d0b1ad
egoing <egoing@gmail.com>
2021-02-04 01:56:34 +0900

    step9-ajax - ajax 기능 추가

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # 이전 작업 
-https://stackblitz.com/edit/react-step-7
+https://stackblitz.com/edit/react-step-8
 
 # 알아야 할 것 
-모듈을 통해서 컴포넌트를 정리 정돈하는 법
+useEffect - 사이드 이팩트란 무엇인가?
+react 에서 ajax 통신하는 방법
+.env.development.template => .env.development 로 변경해서 서버와 통신 할 REACT_APP_API_URL 값을 셋팅해줘야 한다. 
 
 # 성취
-보다 쉽게 애플리케이션의 구조를 파악할 수 있게 되었습니다. 
-보다 컴포넌트를 찾을 수 있게 되었습니다. 
-보다 수정하기는 어렵게 되었습니다. 
+ajax로 동작하는 react app을 만들 었습니다. 
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import Article from "./components/Article";
 import Header from "./components/Header";
@@ -7,38 +7,78 @@ import Create from "./components/Create";
 import Update from "./components/Update";
 import Control from "./components/Control";
 
+var URL = process.env.REACT_APP_API_URL;
+if(URL === undefined){
+  console.error('/.env.development.template => /.env.development')
+}
+function getTopics(setTopics) {
+  fetch(URL + "topics")
+    .then(function(type) {
+      return type.json();
+    })
+    .then(function(result) {
+      setTopics(result);
+    });
+}
+
+function Read(props) {
+  var [topic, setTopic] = useState();
+  useEffect(
+    function() {
+      if (props.id === null) return;
+      fetch(URL + "topics/" + props.id)
+        .then(function(type) {
+          return type.json();
+        })
+        .then(function(result) {
+          setTopic(result);
+        });
+    },
+    [props.id]
+  );
+  if (topic) {
+    return <Article title={topic.title} description={topic.description} />;
+  } else {
+    return <>Loading...</>;
+  }
+}
 export default function App() {
   var [mode, setMode] = useState("WELCOME");
-  var [selectedId, setSelectedId] = useState(1);
+  var [selectedId, setSelectedId] = useState(null);
+  var [topic, setTopic] = useState();
   var [nextId, setNextId] = useState(4);
-  var [topics, setTopics] = useState([
-    { id: 1, title: "html", description: "html is..." },
-    { id: 2, title: "css", description: "css is..." },
-    { id: 3, title: "js", description: "js is..." }
-  ]);
+  var [topics, setTopics] = useState([]);
+  useEffect(function() {
+    getTopics(setTopics);
+  }, []);
+
   var article = null;
   if (mode === "WELCOME") {
     article = <Article title="Welcome" description="Hello, WEB" />;
   } else if (mode === "READ") {
-    for (var i = 0; i < topics.length; i++) {
-      if (topics[i].id === selectedId) {
-        article = (
-          <Article
-            title={topics[i].title}
-            description={topics[i].description}
-          />
-        );
-      }
-    }
+    article = <Read id={selectedId} />;
   } else if (mode === "CREATE") {
     article = (
       <Create
         onCreate={function(data) {
-          data.id = nextId;
-          setTopics([...topics, data]);
-          setSelectedId(nextId);
-          setMode("READ");
-          setNextId(nextId + 1);
+          fetch(URL + "topics", {
+            method: "POST",
+            body: JSON.stringify({
+              title: data.title,
+              description: data.description
+            }),
+            headers: {
+              "Content-Type": "application/json"
+            }
+          })
+            .then(function(type) {
+              return type.json();
+            })
+            .then(function(result) {
+              getTopics(setTopics);
+              setSelectedId(result.id);
+              setMode("READ");
+            });
         }}
       />
     );
@@ -54,6 +94,25 @@ export default function App() {
       <Update
         data={selectedTopic}
         onUpdate={function(data) {
+          fetch(URL + "topics/" + data.id, {
+            method: "PUST",
+            body: JSON.stringify({
+              title: data.title,
+              description: data.description
+            }),
+            headers: {
+              "Content-Type": "application/json"
+            }
+          })
+            .then(function(type) {
+              return type.json();
+            })
+            .then(function(result) {
+              getTopic(setTopics);
+              setSelectedId(result.id);
+              setMode("READ");
+            });
+
           var newTopics = [...topics];
           for (var i = 0; i < newTopics.length; i++) {
             if (newTopics[i].id === data.id) {
@@ -94,7 +153,6 @@ export default function App() {
                 newTopics.push(topics[i]);
               }
             }
-            console.log(newTopics);
             setTopics(newTopics);
             mode = "WELCOME";
           }
